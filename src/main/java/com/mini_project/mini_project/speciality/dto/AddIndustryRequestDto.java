b89129526991fb4bdd3c589be0fa0ee15614d8fd
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-05-11 00:43:59 +0900

    feat: 전문 분야 관련 스킬 및 산업 지식 DTO 클래스 추가
    - AddSkillRequestDto: 대표/일반 스킬 추가용
    - AddIndustryRequestDto: 산업 지식 추가용
    - Delete 요청은 PathVariable 기반으로 처리 예정이라 별도 DTO는 생략

---
+++ src/main/java/com/mini_project/mini_project/speciality/dto/AddIndustryRequestDto.java
@@ -0,0 +1,6 @@
+package com.mini_project.mini_project.speciality.dto;
+
+public class AddIndustryRequestDto {
+
+    private String industry_name;
+}
---
+++ src/main/java/com/mini_project/mini_project/speciality/dto/AddSkillRequestDto.java
@@ -0,0 +1,6 @@
+package com.mini_project.mini_project.speciality.dto;
+
+public class AddSkillRequestDto {
+
+    private String skill_name;
+}
